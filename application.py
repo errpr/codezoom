158ce1440f9e4e5d253d10fd29c6c9a4aa216b0b
Steven B <errorofcamel@gmail.com>
2018-08-21 10:04:00 -0500

    Working register and login

--- application.py
+++ application.py
@@ -1,12 +1,13 @@
 import os
 import re
-from flask import Flask, jsonify, render_template, request, session, redirect, url_for, escape
+from flask import Flask, jsonify, flash, render_template, request, session, redirect, url_for, escape
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.base import Base
 from models.user import User
+import bcrypt
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('sqlite:///app.db', echo=True)
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(engine)
 dbsession = DBSession()
@@ -15,6 +16,7 @@ dbsession.close()
 # Configure application
 app = Flask(__name__)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = False
+app.secret_key = os.environ["FLASK_SECRET"]
 
 # Ensure responses aren't cached
 @app.after_request
@@ -31,44 +33,65 @@ def before_request():
 
 @app.route("/")
 def index():
-    """Render map"""
+    
     return render_template("index.html")
 
-@app.route("/login", methods=["GET", "POST", "DELETE"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        if not request.form.get("inputUsername1"):
+            flash("Username required")
+            return redirect("/login")
+        if not request.form.get("inputPassword1"):
+            flash("Password required")
+            return redirect("/login")
+        exists = dbsession.query(User).filter(User.name == request.form.get("inputUsername1")).first()
+        if exists:
+            if bcrypt.checkpw(request.form.get("inputPassword1").encode(), exists.pw_hash):
+                session["user_id"] = exists.id
+                flash("Successfully logged in")
+                return redirect("/")
+        flash("Incorrect password or username")
+        return redirect("/login")
+
     return render_template("login.html")
 
-@app.route("/register", methods=["GET", "POST", "DELETE"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
 
     if request.method == "POST":
-        if not request.form.get("username"):
+        for element in request.form:
+            print(element)
+        if not request.form.get("inputUsername1"):
             flash("Username required")
             return redirect("/register")
         
-        elif not request.form.get("password"):
+        elif not request.form.get("inputPassword1"):
             flash("Password required")
             return redirect("/register")
         
-        elif not request.form.get("confirm"):
+        elif not request.form.get("inputConfirm1"):
             flash("Password confirmation required")
             return redirect("/register")
         
-        elif not request.form.get("confirm") == request.form.get("password"):
+        elif not request.form.get("inputConfirm1") == request.form.get("inputPassword1"):
             flash("Password must match confirmation")
             return redirect("/register")
 
-        exists = dbsession.query(User).filter(User.name == request.form.get("username"))
-        if len(exists) > 0:
+        exists = dbsession.query(User).filter(User.name == request.form.get("inputUsername1")).first()
+        print(exists)
+        if exists:
             flash("Username already exists")
             return redirect("/register")
 
         #insert user into db and set session
-        new_user = User()
+        pw_hash = bcrypt.hashpw(request.form.get("inputPassword1").encode(), bcrypt.gensalt())
+        new_user = User(name=request.form.get("inputUsername1"), fullname=request.form.get("inputUsername1"), pw_hash=pw_hash)
+        dbsession.add(new_user)
+        dbsession.commit()
 
-        return redirect("/")
-    elif request.method == "DELETE"
-        #remove user from db
+        session["user_id"] = new_user.id
+        flash("Created account and logged in successfully.")
         return redirect("/")
 
     return render_template("register.html")
