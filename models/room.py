f642f7c8eae530d7ed76f441eeee9d6580203e5e
Steven Branson <steve@diodeware.com>
2018-09-24 20:02:02 -0500

    Improve room creation UI

--- models/room.py
+++ models/room.py
@@ -17,6 +17,7 @@ class Room(Base):
     owner_id = Column(Integer, ForeignKey("users.id"))
     time_created = Column(DateTime(timezone=True), server_default=func.now())
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
+    password = Column(String)
 
     owner = relationship("User", back_populates="rooms")
     problems = relationship("RoomProblem", order_by=RoomProblem.order_id)
